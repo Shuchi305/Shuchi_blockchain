a52e4b521c53bc55a2d387a8e8c44327ba92fd81
Shuchi305 <66833303+Shuchi305@users.noreply.github.com>
2022-03-17 20:02:41 +0530

    Update smart_contract.sol

--- smart_contract.sol
+++ smart_contract.sol
@@ -1,13 +1,7 @@
 //SPDX-License-Identifier: --> contracts/1_Storage.sol
 pragma solidity >=0.7.0 <0.9.0;
 //CRUD - Create , Read, Update , Delete
-
-
-//CRUD - Create , Read, Update , Delete
-
-
 contract CRUD{
-
 
     uint idx = 0;
 
@@ -15,32 +9,32 @@ contract CRUD{
         uint id;
         string name;
         string email;
-        uint age;
+        string industry;
     }
 
     User[] user;
     //Create
-    function create(string memory newName,string memory newEmail,uint newAge) public{
-        user.push(User(idx,newName,newEmail,newAge));
+    function create(string memory newName,string memory newEmail,string memory newIndustry) public{
+        user.push(User(idx,newName,newEmail,newIndustry));
         idx += 1;
     }
 
     //Read
-    function readStructure(uint idx) public view returns(uint,string memory,string memory,uint){
+    function readStructure(uint idx) public view returns(uint,string memory,string memory,string memory){
         for (uint i = 0; i < user.length; i++){
             if (user[i].id == idx){
-                return(user[idx].id , user[idx].name , user[idx].email , user[idx].age);
+                return(user[idx].id , user[idx].name , user[idx].email , user[idx].industry);
             }
         }
     }
 
     //Update
-    function update(uint idx , string memory newName, string memory newEmail, uint newAge) public{
+    function update(uint idx , string memory newName, string memory newEmail, string memory newIndustry) public{
         for (uint i = 0; i < user.length; i++){
             if (user[i].id == idx){
                 user[idx].name = newName;
                 user[idx].email = newEmail;
-                user[idx].age = newAge;
+                user[idx].industry = newIndustry;
             }
         }
     }
